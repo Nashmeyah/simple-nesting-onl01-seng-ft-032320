de5044104602ac4d22616d79a19f59a336bbd67f
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-03-28 22:53:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -2,18 +2,24 @@
 def hopper
 	programmer_hash = 
  		{
+      :hopper=>{
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
-        },
+        }
+      },
+      :alan_kay_is_known_for=>{
         :alan_kay => {
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
-        },
+        }
+      },
+      :dennis_ritchies_language=>{
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
         }
+      }
      }
 
 
