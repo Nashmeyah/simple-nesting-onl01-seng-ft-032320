c0e8e8fd6b1c4103b0af9866283e379b7bfb5c3f
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-03-29 00:14:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -138,7 +138,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash.merge[:dennis_ritchie][:languages] = "Assembly"
+     programmer_hash[:dennis_ritchie][:languages] = "Assembly"
 
       return programmer_hash
 end
